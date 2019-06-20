7204c3506f2f241332812be256f9f488163ccfc0
Songer Travis <tsonger20@gmail.com>
2019-06-20 23:34:01 +0000

    Revert "Fix another java.util.NoSuchElementException: No value present"
    This reverts commit 9c956333630713229f65e5d9d66dc731f8d8b335.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Icbca1442b5b7b42c1a7fe3e38d013ab54374ee76

--- src/main/java/com/googlesource/gerrit/plugins/avatars/external/ExternalUrlAvatarProvider.java
+++ src/main/java/com/googlesource/gerrit/plugins/avatars/external/ExternalUrlAvatarProvider.java
@@ -99,7 +99,7 @@ public class ExternalUrlAvatarProvider implements AvatarProvider {
   @Override
   public String getChangeAvatarUrl(IdentifiedUser forUser) {
     String userReplacedAvatarChangeURL = replaceInUrl(USER_PLACEHOLDER,
-        avatarChangeUrl, forUser.getUserName().orElse(null));
+        avatarChangeUrl, forUser.getUserName().get());
     return replaceInUrl(EMAIL_PLACEHOLDER, userReplacedAvatarChangeURL,
         forUser.getAccount().getPreferredEmail());
   }
